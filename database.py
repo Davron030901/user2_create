fec61609107a50013d87e3ce951a2f2f129607c4
Davron <152484311+Davron030901@users.noreply.github.com>
2024-04-07 10:42:45 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
 import sqlalchemy
 
-engine=sqlalchemy.create_engine('sqlite:///movies.db',echo=True)
+engine= sqlalchemy.create_engine('sqlite:///users.db',echo=True)
+
+
+user_to_insert=[{'first_name':'Sarah','last_name':'Pery','email_address':'sarah.perry@gmail.com'},{'first_name':'Felix','last_name':'Martin','email_address':'felix.martin@gmail.com'},{'first_name':'John','last_name':'Patrick','email_address':'john.patrick@gmail.com'},{'first_name':'Jessica','last_name':'Jones','email_address':'jessica.jones@gmail.com'},{'first_name':'Percy','last_name':'Colton','email_address':'percy.colton@gmail.com'}]
 
 with engine.connect() as conn:
-    result=conn.execute(sqlalchemy.text("SELECT * FROM Movies"))
+    conn.execute(sqlalchemy.text('''CREATE TABLE IF NOT EXISTS Users (user_id integer primary key autoincrement,first_name text,last_name text,email_address text)'''))
+    conn.execute(sqlalchemy.text('''INSERT INTO Users (first_name,last_name,email_address) VALUES (:first_name text, :last_name text, :email_address text)'''),user_to_insert)
+
+    result=conn.execute(sqlalchemy.text('''SELECT * FROM Users'''))
     for row in result:
         print(row)
+
+    conn.commit()
